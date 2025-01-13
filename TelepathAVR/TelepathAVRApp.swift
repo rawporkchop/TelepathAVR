49b09e64868eec908f4cc41d7a5dca5e4459f0c0
Oliver Larsson <oliver.k.larsson@gmail.com>
2025-01-12 21:03:47 -0800

    Initial Commit

---
+++ TelepathAVR/TelepathAVRApp.swift
@@ -0,0 +1,17 @@
+//
+//  TelepathAVRApp.swift
+//  TelepathAVR
+//
+//  Created by Oliver Larsson on 1/12/25.
+//
+
+import SwiftUI
+
+@main
+struct TelepathAVRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TelepathAVRTests/TelepathAVRTests.swift
@@ -0,0 +1,17 @@
+//
+//  TelepathAVRTests.swift
+//  TelepathAVRTests
+//
+//  Created by Oliver Larsson on 1/12/25.
+//
+
+import Testing
+@testable import TelepathAVR
+
+struct TelepathAVRTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
